2387bbb9b62e60e9d337da68af1fcb98945bb555
Timothy Haskins <tijohask@iupui.edu>
2016-11-16 12:18:50 -0500

    Forgot the stupid header guards... 2 hours of work to find it. Stupid.

--- Num_Node.h
+++ Num_Node.h
@@ -3,10 +3,12 @@
 // I pledge that I have neither given nor receieved any help
 // on this assignment.
 
+#ifndef _NUM_NODE_H_
+#define _NUM_NODE_H_
+
 #include "Stack.h"
 #include "Node.h"
 
-// The top Node object that all other Node objects will be inheriting from.
 class Num_Node : public Node
 {
 public:
@@ -23,3 +25,5 @@ private:
 //	virtual bool execute (Stack <int> & stack) = 0;
 //	virtual int precedence() = 0;
 };
+
+#endif
